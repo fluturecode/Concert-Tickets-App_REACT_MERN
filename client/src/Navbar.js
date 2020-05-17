e7834aa373a5fbb9c298e043210b8c8e3e3d9fc4
FreddyB77 <fjbain77@gmail.com>
2020-05-17 05:58:30 -0400

    pushing 90% correct

---
+++ client/src/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from "./420eventlogo.png";
+
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+            <img src={logo} alt="EventedLogo" style={{width: "150px"}}/>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <nav class="navbar navbar-light bg-dark">
+            <span class="navbar-text">
+            <h3 style={{ textAlign: "center" }}>City Place Location</h3>
+            </span>
+        </nav>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar
