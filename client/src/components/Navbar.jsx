49972319086695c195bc360e824c07d5464840ec
ghostkid130 <urena.reynold+git@gmail.com>
2020-05-20 02:51:30 -0400

    pushing enhancement-20

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -30,24 +30,6 @@ const Navbar = () => {
           <li class="nav-item">
             <a class="nav-link">About Us</a>
           </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item">Action</a>
-              <a class="dropdown-item">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item">Something else here</a>
-            </div>
-          </li>
         </ul>
       </div>
     </nav>
